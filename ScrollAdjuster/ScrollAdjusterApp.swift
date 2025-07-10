e4ef15e85a21c222d676f8c939eb70023fd6cb79
Tanaka Mikihisa <minemiki0901@gmail.com>
2025-07-11 07:07:54 +0900

    Initial Commit

---
+++ ScrollAdjuster/ScrollAdjusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollAdjusterApp.swift
+//  ScrollAdjuster
+//
+//  Created by tanakamiki on 2025/07/11.
+//
+
+import SwiftUI
+
+@main
+struct ScrollAdjusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
