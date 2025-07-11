8a7f62faba8d6ed0ca8f4e8a54c7d77f5678d800
Tanaka Mikihisa <minemiki0901@gmail.com>
2025-07-12 01:21:12 +0900

    7.12.1:21

--- ScrollAdjuster/ScrollWatcher.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-
-struct ScrollWatcher {
-    var scrollOffset: CGFloat = 0 // スクロール位置を監視
-    var scrollStopTask: DispatchWorkItem?  //遅延アクション管理
-    var scrollViewWidth : CGFloat = 0
-}
